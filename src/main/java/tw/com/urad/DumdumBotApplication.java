91840d15838656f98afcc9c0b450e52a9c5bf3ba
Jerry Lai <jerry@urad.com.tw>
2017-01-25 16:16:34 +0800

    Remove auto configuration

--- src/main/java/tw/com/urad/DumdumBotApplication.java
+++ src/main/java/tw/com/urad/DumdumBotApplication.java
@@ -1,15 +1,10 @@
 package tw.com.urad;
 
-import com.linecorp.bot.spring.boot.LineBotProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
-@EnableAutoConfiguration
-@EnableConfigurationProperties(LineBotProperties.class)
 @SpringBootApplication
 public class DumdumBotApplication {
 
